396574e077bde606b38c72e2d9259052828defb3
David Konsumer <konsumer@jetboystudio.com>
2020-08-14 04:34:33 -0700

    use rot

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ const sharp = require('sharp')
 const plane = Buffer.from(require('./plane.json').data)
 
 module.exports = async (req, res) => {
-  const { rot, alt = 3000, hex, fmt } = req.query
+  const { rot = 0, alt = 3000, hex, fmt } = req.query
 
   res.setHeader('content-type', `image/${fmt || 'png'}`)
   // TODO: actually figure these out based on data
   const img = await sharp(plane)
-    .rotate(90)
+    .rotate(rot)
     .resize(16 * (alt / 3000) | 0)
     .toBuffer()
   res.send(img)
