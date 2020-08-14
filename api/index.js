e6245cdda718506ea1ae60b588e5052101360760
David Konsumer <konsumer@jetboystudio.com>
2020-08-14 05:10:24 -0700

    better transparent

--- api/index.js
+++ api/index.js
@@ -7,8 +7,8 @@ module.exports = async (req, res) => {
   res.setHeader('content-type', `image/${fmt || 'png'}`)
   // TODO: actually figure these out based on data
   const img = await sharp(plane)
-    .rotate(rot)
     .resize(16 * (alt / 3000) | 0)
+    .rotate(rot | 0, { background: "transparent" })
     .toBuffer()
   res.send(img)
 }
